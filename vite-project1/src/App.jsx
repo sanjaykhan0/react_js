4400416491074a0a2e2a5a45db1d3dfe126591a1
sanjaykhan0 <sanjaykhan2228@gmail.com>
2024-08-30 15:49:23 +0530

    life circle

---
+++ vite-project1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Classcom from './Componant/Classcom'
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+
+    <Classcom/>
+
+    </div>
+  )
+}
