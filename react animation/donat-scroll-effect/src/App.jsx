84b35d7dd94f28df04141f8db703f7c07d75feb8
sanjaykhan0 <sanjaykhan2228@gmail.com>
2024-09-05 12:34:42 +0530

    Counterp-pr

---
+++ react animation/donat-scroll-effect/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Canvas} from "@react-three/fiber"
+
+export default function App() {
+  return (
+    
+ <Canvas>
+  <mesh>
+    <boxGeometry/>
+    <meshBasicMaterial/>
+    
+  </mesh>
+
+ </Canvas>
+   
+  )
+}
