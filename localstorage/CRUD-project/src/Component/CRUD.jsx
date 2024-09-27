b82853192c65fe2e21d10fe18d0a97d250384271
sanjaykhan0 <sanjaykhan2228@gmail.com>
2024-09-27 11:49:36 +0530

    crud

--- localstorage/CRUD-project/src/Component/CRUD.jsx
+++ localstorage/CRUD-project/src/Component/CRUD.jsx
@@ -7,35 +7,59 @@ export default function CRUD() {
     const [sub,setSub]=useState('')
 
     const [data,setData]=useState([])
+    const [index,setIndex]=useState()
   
     useEffect(()=>{
-      let storedData = JSON.parse(localStorage.getItem("student"))||[]
-      setData(storedData)
+      let storeData = JSON.parse(localStorage.getItem("student"))||[]
+      setData(storeData)
     },[])
 
 
     let handleclick = ()=>{
-      let newUser =[{id:data.length+1,name,sub}]
-      // data.push(user)
-      let newdata = [...data,newUser]
-      setData(newdata)
-      localStorage.setItem("student",JSON.stringify(newdata)) ;
-        
-      // console.log(newdata)
+      
+      let newStudent = {id:data.length+1,name:name,sub:sub}
+      let storeData = JSON.parse(localStorage.getItem("student"))||[]
+      if(index){
+         let updataData = storeData.find((e)=>e.id == index)
+         updataData.id = index
+         updataData.name = name
+         updataData.sub = sub
+         localStorage.setItem("student",JSON.stringify(storeData))
+         setIndex(null)
+
+
+      }
+      else{
+        storeData.push(newStudent)  
+      localStorage.setItem("student",JSON.stringify(storeData))
+      setData(storeData)
+      }
+      console.log(storeData);
+      
 
     }
 
-    const handeldel = (id) => {
-      const deldata = data.filter((item) => item.id !==id);
-      setData(deldata);
-      localStorage.setItem("student", JSON.stringify(deldata)); 
-    };
+  let handleDelete = (id)=>{
+    let storeData = JSON.parse(localStorage.getItem("student"))||[]
+    storeData =storeData.filter((e)=> e.id != id )
+    localStorage.setItem("student",JSON.stringify(storeData))
+    setData(storeData)
+  }
 
+  let handleEdit = (id)=>{
+    let storeData = JSON.parse(localStorage.getItem("student"))||[]
+    let editdata = storeData.find((e)=> e.id == id)
+    console.log(editdata)
+    setIndex(editdata)
+    setName(editdata.name)
+    setSub(editdata.sub)
+
+  }
   return (
     <div>
-      <input type="text" placeholder='name' onChange={(e)=>setName(e.target.value)} />
-      <input type="text" placeholder='subject' onChange={(e)=>setSub(e.target.value)}/>
-      <button onClick={handleclick} >submit</button>
+      <input type="text" value={name} placeholder='name' onChange={(e)=>setName(e.target.value)} />
+      <input type="text" value={sub} placeholder='subject' onChange={(e)=>setSub(e.target.value)}/>
+      <button onClick={handleclick} >{index?"update":"addData"}</button>
 
       <table border={2} width={1000} >
         <thead>
@@ -52,8 +76,8 @@ export default function CRUD() {
               <td>{e.name}</td>
               <td>{e.sub}</td>
               <td colSpan={2}>
-                <button color='green'>Edit</button>
-                <button color='red' onClick={()=>handeldel(e.id)}>Del</button>
+                <button color='green' onClick={()=>handleEdit(e.id)}>Edit</button>
+                <button color='red' onClick={()=>handleDelete(e.id)}>Del</button>
               </td>
 
             </tr>
