1a5993b477e3e44be8d69a582c5a5e6d04361868
sanjaykhan0 <162433584+sanjaykhan0@users.noreply.github.com>
2024-11-13 15:41:48 +0530

    Update App.jsx

--- login-form-firebase/src/App.jsx
+++ login-form-firebase/src/App.jsx
@@ -4,14 +4,23 @@ import Dashboard from './component/Dashboard';
 import { SignIn } from './component/Singin';
 import { SignUp } from './component/SingUp';
 import Toggle from './component/Toggle';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
     <div>
- {/* <SignUp/> */}
- {/* <SignIn/> */}
- {/* <Toggle/> */}
- <Dashboard/>
+      {/* <SignUp/> */}
+      {/* <SignIn/> */}
+      {/* <Toggle/> */}
+      {/* <Dashboard/> */}
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Toggle/>}></Route>
+          <Route path='/dashboard' element={<Dashboard/>}></Route>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
