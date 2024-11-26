d377af366a2b0dc444d06a714fb2f3af004e4062
sanjaykhan0 <sanjaykhan2228@gmail.com>
2024-11-26 12:31:11 +0530

    portfolio

--- portfolio-react-final-project/src/components/About.jsx
+++ portfolio-react-final-project/src/components/About.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { motion } from "framer-motion"; ~
+import React, { useRef, useEffect } from "react";
+import "./About.css";
+import { web } from "../assets";
 
 export default function About() {
   const styles = {
@@ -17,11 +18,60 @@ export default function About() {
     sectionSubText:
       "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
   };
+
+  const cardRef = useRef(null);
+  const glareRef = useRef(null);
+  let cachedCardDomRect = null;
+
+  useEffect(() => {
+    const card = cardRef.current;
+    const glare = glareRef.current;
+
+    card.onmouseenter = (e) =>
+      (cachedCardDomRect = e.target.getBoundingClientRect());
+
+    card.onmouseleave = (e) => {
+      card.animate(
+        { transform: "rotateX(0deg) rotateY(0deg)" },
+        { duration: 250, fill: "forwards", delay: 50 }
+      );
+    };
+
+    card.onmousemove = (e) => {
+      if (!cachedCardDomRect) return;
+
+      let xCoord = e.clientX - cachedCardDomRect.left;
+      let yCoord = e.clientY - cachedCardDomRect.top;
+      let xDegreesToRotate =
+        (20 - 20 * (yCoord / cachedCardDomRect.height)) * -1;
+      let yDegreesToRotate = 20 - 40 * (xCoord / cachedCardDomRect.width);
+
+      card.animate(
+        {
+          transform: `rotateX(${xDegreesToRotate}deg) rotateY(${yDegreesToRotate}deg)`,
+        },
+        { duration: 500, fill: "forwards", delay: 50 }
+      );
+
+      glare.animate(
+        {
+          transform: `translateX(${xCoord - 500}px) translateY(${
+            yCoord - 500
+          }px)`,
+        },
+        { duration: 0, fill: "forwards" }
+      );
+    };
+  }, []);
+
   return (
-    <div >
-      <div className={`intro px-52 w-full flex flex-col items-start py-5 bg-primary mt-20 `}>
+    <div>
+      <div
+        className={`intro px-52 w-full flex flex-col items-start py-5 bg-primary mt-20 `}
+      >
         <font className="text-secondary">introduction</font>
-        <h1 className="text-white text-[40px] font-bold">Overview</h1><br />
+        <h1 className="text-white text-[40px] font-bold">Overview</h1>
+        <br />
         <p className="w-2/4 text-secondary ">
           As a Full Stack Developer, I create responsive web applications using
           React.js for front-end and add more functionality like a
@@ -30,8 +80,24 @@ export default function About() {
           entire stack for efficient, user-friendly applications.
         </p>
       </div>
-      <div className=""></div>
 
+      {/* card  */}
+      <div className="">
+        <section>
+          <div className="container">
+            
+              <div ref={cardRef} className="card border"  style={{  padding:"50px 0px "}}>
+                <div className="card_thumbnail" >
+                  <img src={web} style={{ height: "70px", }} />
+                  <div className="card_thumbmail_shadow"></div>
+                </div>
+                <p className="card_caption font-[10px]" style={{  padding:"0px 10px ",boxSizing:"border-box"}}>Web Developer </p>
+                <div ref={glareRef} className="glare"></div>
+              </div>
+            
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
